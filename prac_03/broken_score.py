34b22278b6c46fe127b6f85508170c7a29bb8c14
PeterMenzel <peter.menzel@my.jcu.edu.au>
2017-08-22 21:27:13 +1000

    Update the temperatures and broken_score programs in line with prac_03 standards.

---
+++ prac_03/broken_score.py
@@ -0,0 +1,27 @@
+"""
+CP1404/CP5632 - Practical
+Broken program to determine score status
+"""
+
+
+def main():
+    score = float(input("Enter score: "))
+    result = get_result(score)
+    print(result)
+
+
+def get_result(score):
+    if score < 0:
+        result = "Invalid score"
+    elif score > 100:
+        result = "Invalid score"
+    elif score >= 90:
+        result = "Excellent"
+    elif score >= 50:
+        result = "Passable"
+    else:
+        result = "Bad"
+    return result
+
+
+main()
