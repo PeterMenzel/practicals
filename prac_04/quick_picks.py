9c11209981d6d054d329e2296920bcf636e90047
PeterMenzel <peter.menzel@my.jcu.edu.au>
2017-08-28 17:27:32 +1000

    Complete prac_04 intermediate exercises.

---
+++ prac_04/quick_picks.py
@@ -0,0 +1,25 @@
+import random
+
+NUMBERS_PER_LINE = 6
+MIN = 1
+MAX = 45
+
+
+def main():
+    number_of_quick_picks = int(input("How many quick picks would you like to generate? "))
+
+    while number_of_quick_picks < 0:
+        number_of_quick_picks = int(input("How many quick picks would you like to generate? "))
+
+    for i in range(number_of_quick_picks):
+        quick_picks = []
+        for j in range(NUMBERS_PER_LINE):
+            number = random.randint(MIN, MAX)
+            while number in quick_picks:
+                number = random.randint(MIN, MAX)
+            quick_picks.append(number)
+        quick_picks.sort()
+        print(" ".join("{:2}".format(number) for number in quick_picks))
+
+
+main()
