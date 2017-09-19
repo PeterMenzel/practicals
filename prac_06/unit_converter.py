92da37e01c9e7ff7f8c83eb4774012e1dbe02732
PeterMenzel <peter.menzel@my.jcu.edu.au>
2017-09-20 00:36:32 +1000

    Enabled error checking for unit_converting program for prac_06.

--- prac_06/unit_converter.py
+++ prac_06/unit_converter.py
@@ -8,12 +8,22 @@ class UnitConverter(App):
         self.root = Builder.load_file('unit_converter.kv')
         return self.root
 
-    def handle_increment(self, value, increment):
+    def handle_increment(self, input_value, increment):
+        value = self.error_check(input_value)
         value += increment
         self.root.ids.input_number.text = str(value)
 
-    def handle_convert_miles_to_km(self, value):
+    def handle_convert_miles_to_km(self, input_value):
+        value = self.error_check(input_value)
         self.root.ids.output_number.text = str(value * 1.609344)
+
+    def error_check(self, input_value):
+        try:
+            value = float(input_value)
+        except:
+            value = float(0)
+            self.root.ids.output_number.text = str("0.0")
+        return value
 
 
 UnitConverter().run()
