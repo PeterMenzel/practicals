ac6384fe8f1856e39ae101cee6d480448002f025
PeterMenzel <peter.menzel@my.jcu.edu.au>
2017-09-20 01:14:41 +1000

    Add automatic calculation and proper formatting for unit_converting program for prac_06.

--- prac_06/unit_converter.py
+++ prac_06/unit_converter.py
@@ -9,18 +9,18 @@ class UnitConverter(App):
         return self.root
 
     def handle_increment(self, input_value, increment):
-        value = self.error_check(input_value)
+        value = self.perform_error_check(input_value)
         value += increment
         self.root.ids.input_number.text = str(value)
 
     def handle_convert_miles_to_km(self, input_value):
-        value = self.error_check(input_value)
+        value = self.perform_error_check(input_value)
         self.root.ids.output_number.text = str(value * 1.609344)
 
-    def error_check(self, input_value):
+    def perform_error_check(self, input_value):
         try:
             value = float(input_value)
-        except:
+        except ValueError:
             value = float(0)
             self.root.ids.output_number.text = str("0.0")
         return value
