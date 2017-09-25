ae60579422de97145b7c73626e622d57eaf5529a
PeterMenzel <peter.menzel@my.jcu.edu.au>
2017-09-25 20:53:57 +1000

    Update and complete programming_languages program for prac_07.

--- prac_07/languages.py
+++ prac_07/languages.py
@@ -8,7 +8,12 @@ def main():
     python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
     visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
 
-    print(ruby)
+    print(python)
+    languages = [ruby, python, visual_basic]
+    print("The dynamically typed languages are:")
+    for language in languages:
+        if language.is_dynamic():
+            print(language.name)
 
 
 main()
