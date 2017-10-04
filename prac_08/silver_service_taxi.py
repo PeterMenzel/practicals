6f42dbd860c538ef710c3a0c1037727ccb9f79bd
PeterMenzel <peter.menzel@my.jcu.edu.au>
2017-10-04 14:47:35 +1000

    Add silver_service_taxi program for prac_08.

---
+++ prac_08/silver_service_taxi.py
@@ -0,0 +1,17 @@
+
+from prac_08.taxi import Taxi
+
+
+class SilverServiceTaxi(Taxi):
+
+    flagfall = 4.50
+
+    def __init__(self, name, fuel, fanciness):
+        super().__init__(name, fuel)
+        self.fanciness = fanciness
+        self.price_per_km *= self.fanciness
+
+    def __str__(self):
+        return "{}, {}km on current fare, ${:.2f}/km plus flagfall of ${:.2f}".format(super().__str__(),
+                                                                                      self.current_fare_distance,
+                                                                                      self.price_per_km, self.flagfall)
